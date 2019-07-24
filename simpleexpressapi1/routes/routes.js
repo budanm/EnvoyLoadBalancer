a4eeaa11b7fa48f9e9e2a9863c0df9defc260d38
i312345 <soumya.mukhopadhyay@sap.com>
2019-07-24 10:40:16 +0530

    adding new routes

--- simpleexpressapi1/routes/routes.js
+++ simpleexpressapi1/routes/routes.js
@@ -4,6 +4,10 @@ var appRouter = function (app) {
         res.send("Hello I am a simeple express api service 1");
     });
 
+    app.get("/service/1/subpath", function (req, res) {
+        res.send("Hello I am a simeple express api service 1 subpath");
+    });
+
 }
 
 module.exports = appRouter;
